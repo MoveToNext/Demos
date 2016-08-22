c71ccbbb121896504481ce0af838b4f7cc1d521a
Liuyang <505169591@qq.com>
2016-08-22 15:42:51 +0800

    camera

--- app/src/main/java/liu/myapplication/MainActivity.java
+++ app/src/main/java/liu/myapplication/MainActivity.java
@@ -1,37 +1,48 @@
 package liu.myapplication;
 
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
-import android.graphics.Color;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.KeyEvent;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.PopupWindow;
-import android.widget.TextView;
 
 import com.orhanobut.logger.Logger;
+
+import java.io.File;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import liu.myapplication.ui.AnimationActivity;
+import liu.myapplication.ui.ClipImageActivity;
 import liu.myapplication.ui.CustomViewActivity;
 import liu.myapplication.ui.ExpandableListViewActivity;
 import liu.myapplication.ui.GenericActivity;
@@ -42,9 +53,8 @@ import liu.myapplication.ui.PullToRefreshListActivity;
 import liu.myapplication.ui.RecyclerDefaultActivity;
 import liu.myapplication.ui.RecyclerViewActivity;
 import liu.myapplication.ui.RemoteViewActivity;
-import liu.myapplication.view.MyView;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.custom_view)
     Button customView;
@@ -75,11 +85,18 @@ public class MainActivity extends ActionBarActivity {
     @BindView(R.id.toolbar)
     Toolbar toolbar;
     @BindView(R.id.id_nv_menu)
-    NavigationView  MnavigationView;
+    NavigationView MnavigationView;
     @BindView(R.id.id_drawer_layout)
     DrawerLayout MdrawerLayout;
     private int width;
     private int height;
+    private PopupWindow popupWindow;
+    private Context context = this;
+    private final static int REQUEST_CAPTURE = 100;
+    private final static int REQUEST_PICK = 101;
+    private final static int REQUEST_CROP_PHOTO = 102;
+    private File tempFile;
+    private ImageView id_username;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -112,9 +129,41 @@ public class MainActivity extends ActionBarActivity {
                 return true;
             }
         });
+
+        createCameraTempFile(savedInstanceState);
+    }
+
+    private void createCameraTempFile(Bundle savedInstanceState) {
+
+        if (savedInstanceState != null && savedInstanceState.containsKey("tempFile")){
+            tempFile = (File) savedInstanceState.getSerializable("tempFile");
+        }else {
+            tempFile = new File(checkDirPath(Environment.getExternalStorageDirectory().getPath()+"/image/"),System.currentTimeMillis()+".jpg");
+        }
+    }
+
+    private String checkDirPath(String dirPath) {
+        if (TextUtils.isEmpty(dirPath)){
+            return "";
+        }
+        File dir = new File(dirPath);
+        if (!dir.exists()){
+            dir.mkdirs();
+        }
+        return dirPath;
     }
 
     private void initNavigationViewListener() {
+        View inflateHeaderView = MnavigationView.inflateHeaderView(R.layout.header_just_username);
+        id_username = (ImageView) inflateHeaderView.findViewById(R.id.id_userpic);
+
+        id_username.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showPopupWin();
+                MdrawerLayout.closeDrawers();
+            }
+        });
         MnavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(MenuItem item) {
@@ -136,7 +185,7 @@ public class MainActivity extends ActionBarActivity {
 
     private void initDrawLayout() {
         ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this,
-                MdrawerLayout,toolbar,R.string.app_name,R.string.app_name);
+                MdrawerLayout, toolbar, R.string.app_name, R.string.app_name);
         actionBarDrawerToggle.syncState();
         MdrawerLayout.setDrawerListener(actionBarDrawerToggle);
     }
@@ -243,24 +292,121 @@ public class MainActivity extends ActionBarActivity {
     }
 
     private void showPopupWin() {
-        PopupWindow p = new PopupWindow(this);
-        TextView t = new TextView(this);
-        t.setText("i am a pop");
-        t.setTextColor(Color.BLACK);
+        View inflate = LayoutInflater.from(this).inflate(R.layout.layout_popupwindow, null);
+        popupWindow = new PopupWindow(inflate, WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT);
         //实例化一个ColorDrawable颜色为半透明
         ColorDrawable dw = new ColorDrawable(0xb0000000);
         //设置SelectPicPopupWindow弹出窗体的背景
-        p.setBackgroundDrawable(dw);
-        p.setContentView(new MyView(this));
-        p.setWidth(WindowManager.LayoutParams.WRAP_CONTENT);
-        p.setHeight(WindowManager.LayoutParams.WRAP_CONTENT);
-        Logger.d("chidfdsfsdfds");
-        p.showAtLocation(this.popupwindow, Gravity.BOTTOM, 0, 0);
+        popupWindow.setBackgroundDrawable(dw);
+        popupWindow.setTouchable(true);
+        popupWindow.setOutsideTouchable(true);
+        final WindowManager.LayoutParams params = getWindow().getAttributes();
+        params.alpha = 0.5f;
+        getWindow().setAttributes(params);
+        popupWindow.showAtLocation(this.popupwindow, Gravity.BOTTOM, 0, 0);
+        /** 拍照 */
+        inflate.findViewById(R.id.btn_camera).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(tempFile));
+                startActivityForResult(intent, REQUEST_CAPTURE);
+                popupWindow.dismiss();
+            }
+        });
+        /** 从相册选择 */
+        inflate.findViewById(R.id.btn_photo).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //跳转到调用系统图库
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(Intent.createChooser(intent, "请选择图片"), REQUEST_PICK);
+                popupWindow.dismiss();
+            }
+        });
+        /**  取消 */
+        inflate.findViewById(R.id.btn_cancel).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                popupWindow.dismiss();
+            }
+        });
+        popupWindow.setOnDismissListener(new PopupWindow.OnDismissListener() {
+            @Override
+            public void onDismiss() {
+                params.alpha = 1.0f;
+                getWindow().setAttributes(params);
+            }
+        });
     }
 
     @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        switch (requestCode){
+            case REQUEST_CAPTURE:
+                if (resultCode == RESULT_OK){
+                    gotoClipActivity(Uri.fromFile(tempFile));
+                }
+                break;
+            case REQUEST_PICK:  //调用系统相册返回
+                if (resultCode == RESULT_OK) {
+                    Uri uri = data.getData();
+                    gotoClipActivity(uri);
+                }
+                break;
+            case REQUEST_CROP_PHOTO:
+                if (resultCode == RESULT_OK){
+                    if (data.getData() != null){
+                        Uri pic = data.getData();
+                        Logger.d("data.getData() != null");
+                        String cropImagePath = getRealFilePathFromUri(getApplicationContext(), pic);
+                        Bitmap bitMap = BitmapFactory.decodeFile(cropImagePath);
+                        id_username.setImageBitmap(bitMap);
+                    }
+                    Logger.d("data.getData() = null");
+                }
+                break;
+
+        }
+    }
+
+    private String getRealFilePathFromUri(Context applicationContext, Uri uri) {
+        if (null == uri) return null;
+        final String scheme = uri.getScheme();
+        String data = null;
+        if (scheme == null)
+            data = uri.getPath();
+        else if (ContentResolver.SCHEME_FILE.equals(scheme)) {
+            data = uri.getPath();
+        } else if (ContentResolver.SCHEME_CONTENT.equals(scheme)) {
+            Cursor cursor = context.getContentResolver().query(uri, new String[]{MediaStore.Images.ImageColumns.DATA}, null, null, null);
+            if (null != cursor) {
+                if (cursor.moveToFirst()) {
+                    int index = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
+                    if (index > -1) {
+                        data = cursor.getString(index);
+                    }
+                }
+                cursor.close();
+            }
+        }
+        return data;
+    }
+
+    private void gotoClipActivity(Uri uri) {
+        if (uri == null){
+            return;
+        }
+        Intent intent = new Intent(this, ClipImageActivity.class);
+        intent.setData(uri);
+        startActivityForResult(intent, REQUEST_CROP_PHOTO);
+    }
+
+
+    @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if (keyCode == KeyEvent.KEYCODE_BACK){
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
             final AlertDialog.Builder builder = new AlertDialog.Builder(this);
             builder.setTitle("友情提示");
             builder.setMessage("确认退出应用吗");
---
+++ app/src/main/java/liu/myapplication/ui/ClipImageActivity.java
@@ -0,0 +1,109 @@
+package liu.myapplication.ui;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import liu.myapplication.R;
+import liu.myapplication.view.ClipViewLayout;
+
+/**
+ * @PackageName: liu.myapplication.ui
+ * @Description:
+ * @author:
+ * @date: 2016/8/22 14:32
+ */
+public class ClipImageActivity extends AppCompatActivity {
+    @BindView(R.id.iv_back)
+    ImageView ivBack;
+    @BindView(R.id.stock_name)
+    TextView stockName;
+    @BindView(R.id.clipViewLayout1)
+    ClipViewLayout clipViewLayout1;
+    @BindView(R.id.clipViewLayout2)
+    ClipViewLayout clipViewLayout2;
+    @BindView(R.id.btn_cancel)
+    TextView btnCancel;
+    @BindView(R.id.bt_ok)
+    TextView btOk;
+    @BindView(R.id.bottom)
+    RelativeLayout bottom;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clip_image);
+        ButterKnife.bind(this);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+            //设置图片资源
+            clipViewLayout2.setImageSrc(getIntent().getData());
+    }
+
+    @OnClick({R.id.iv_back, R.id.stock_name, R.id.clipViewLayout1, R.id.clipViewLayout2, R.id.btn_cancel, R.id.bt_ok, R.id.bottom})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.iv_back:
+                finish();
+                break;
+            case R.id.btn_cancel:
+                finish();
+                break;
+            case R.id.bt_ok:
+                generateUriAndReturn();
+                break;
+        }
+    }
+
+    private void generateUriAndReturn() {
+        //调用返回剪切图
+        Bitmap zoomedCropBitmap = clipViewLayout2.clip();
+        if (zoomedCropBitmap == null){
+            return;
+        }
+        Uri mSaveUri = Uri.fromFile(new File(getCacheDir(), "cropped_" + System.currentTimeMillis() + ".jpg"));
+        if (mSaveUri != null){
+            OutputStream outputStream = null;
+            try {
+                outputStream = getContentResolver().openOutputStream(mSaveUri);
+                if (outputStream != null){
+                    zoomedCropBitmap.compress(Bitmap.CompressFormat.JPEG, 90, outputStream);
+                }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }finally {
+                if (outputStream != null){
+                    try {
+                        outputStream.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+
+        Intent intent = new Intent();
+        intent.setData(mSaveUri);
+        setResult(RESULT_OK,intent);
+        finish();
+    }
+}
