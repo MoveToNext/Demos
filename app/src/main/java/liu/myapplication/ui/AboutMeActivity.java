dd960cbe7fa280bdbf8c8a4d1022b2372cd88e19
Liuyang <505169591@qq.com>
2016-09-29 18:17:18 +0800

    add ConstraintLayout

---
+++ app/src/main/java/liu/myapplication/ui/AboutMeActivity.java
@@ -0,0 +1,21 @@
+package liu.myapplication.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import liu.myapplication.R;
+import liu.myapplication.view.BaseActivity;
+
+/**
+ * @PackageName: liu.myapplication.ui
+ * @Description:
+ * @author:
+ * @date: 2016/9/29 17:47
+ */
+public class AboutMeActivity extends BaseActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.test_constraintlayout);
+    }
+}
