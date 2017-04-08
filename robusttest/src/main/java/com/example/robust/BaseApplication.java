4db038f38beaa3378cd941b80955a2a5ba439ffe
liutongyang <liutongyang@liutongyangdeMacBook-Pro.local>
2017-04-08 23:44:57 +0800

    robust run ok!

---
+++ robusttest/src/main/java/com/example/robust/BaseApplication.java
@@ -0,0 +1,32 @@
+package com.example.robust;
+
+import android.app.Application;
+
+import com.zhy.http.okhttp.OkHttpUtils;
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+
+/**
+ * Created by liutongyang on 2017/4/8.
+ */
+
+public class BaseApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        initOkhttpClient();
+    }
+
+    private void initOkhttpClient() {
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+//                .addInterceptor(new LoggerInterceptor("TAG"))
+                .connectTimeout(10000L, TimeUnit.MILLISECONDS)
+                .readTimeout(10000L, TimeUnit.MILLISECONDS)
+                //其他配置
+                .build();
+        OkHttpUtils.initClient(okHttpClient);
+    }
+}
--- robusttest/src/main/java/com/example/robust/MainActivity.java
+++ robusttest/src/main/java/com/example/robust/MainActivity.java
@@ -1,13 +1,88 @@
 package com.example.robust;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.meituan.robust.Patch;
+import com.meituan.robust.PatchExecutor;
+import com.meituan.robust.RobustCallBack;
+import com.meituan.robust.patch.annotaion.Modify;
+import com.zhy.http.okhttp.OkHttpUtils;
+import com.zhy.http.okhttp.callback.FileCallBack;
+
+import java.io.File;
+
+import okhttp3.Call;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        text = (TextView) findViewById(R.id.text3);
+        showText();
+        getPatchServer();
+
+    }
+
+    @Modify
+    private void showText() {
+        text.setText("我是修复后的");
+    }
+
+    /**
+     * 从服务器下拉补丁文件
+     */
+    private void getPatchServer() {
+        OkHttpUtils.get().url("http://oo0zaukkd.bkt.clouddn.com/patch.jar")
+                .build()
+                .execute(new FileCallBack(Environment.getExternalStorageDirectory().getPath()+ File.separator+"robust","patch.jar") {
+                    @Override
+                    public void onError(Call call, Exception e, int id) {
+                        Toast.makeText(getApplicationContext(),e.getMessage(),Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onResponse(File response, int id) {
+                        Log.d("MainActivity", "response.getAbsoluteFile():" + response.getAbsoluteFile());
+                        initPatch();
+                    }
+                });
+    }
+
+    private void initPatch() {
+        new PatchExecutor(getApplicationContext(), new PatchManipulateImp(), new RobustCallBack() {
+            @Override
+            public void onPatchListFetched(boolean result, boolean isNet) {
+
+            }
+
+            @Override
+            public void onPatchFetched(boolean result, boolean isNet, Patch patch) {
+
+            }
+
+            @Override
+            public void onPatchApplied(boolean result, Patch patch) {
+
+            }
+
+            @Override
+            public void logNotify(String log, String where) {
+
+            }
+
+            @Override
+            public void exceptionNotify(Throwable throwable, String where) {
+
+            }
+        }).start();
     }
 }
